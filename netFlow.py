7f8470b3867f283d3c6b6c87cb1bf9d9581a713a
Scott <mauicormac@gmail.com>
2016-05-15 16:32:54 -0500

    Deleting Files

--- netFlow.py
+++
@@ -1,5 +0,0 @@
-#!/usr/python
-
-import subprocess
-
-pcap = subprocess.call(["tcpdump", "-r", "/Users/helloroot/Documents/laughing-spork/first_contact.pcap"])
--- netfFlow.bash
+++
@@ -1,9 +0,0 @@
-SRC IP = tcpdump -n -r first_contact.pcap | awk '{print $3}' | sort | uniq -c
-
-DST IP = tcpdump -n -r first_contact.pcap | awk '{print $5}' | sort | uniq -c
-
-SRC AND DST IP = tcpdump -m -r first_contact.pcap | awk '{print $3, $5}' | sort | uniq -c
-
-RECORD AND SAVE TO TIMESTAMPED FILE EVERY 5 MINUTES - tcpdump -G 300 -w %T
-
-a 
