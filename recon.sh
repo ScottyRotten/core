604c221dfcdf90589c29f2095e269b40cb5f57d3
Scott <mauicormac@gmail.com>
2017-01-13 09:11:47 -0600

    Update recon.sh
    created loop to scrape commands from defined array and edited output formatting

--- recon.sh
+++ recon.sh
@@ -4,13 +4,16 @@
 # system configuration scraping
 
 
-# Output File Name
+# Setup Variables and Arrays
 OF=output
 LOGFILES=logconfs
+INFOGRAB=("hostname" "uname -a" "netstat -plant")
+
 
 # Set-up files
 touch /tmp/$OF
 mkdir /tmp/$LOGFILES
+
 
 # Check to see if root
 if [ `whoami` == root ]; then
@@ -20,17 +23,26 @@ else
 	exit 0
 fi
 
+
 # Target to send files to
 echo -n "IP Address to send results to followed by [Enter]:"
 read TARGET
 
-# Primary Scrape
-hostname >> $OF
-uname -a >> $OF
-cat /etc/*.conf >> $OF
-netstat -plant >> $OF
+
+for i in "${INFOGRAB[@]}"; do
+	echo "[+] Sending data from $i to $OF";
+	printf "\n" >> $OF
+	echo "[# # # # # # FILENAME: $i # # # # # #" >> $OF;
+	printf "\n" >> $OF
+	$i >> $OF;
+done
+
+
+
+# File Grab
 
 for i in `find /var/log/*.log | cut -f 4 -d /`; do
+	echo "[+] Copying file to /tmp/$LOGFILES/$i";
 	cp /var/log/$i /tmp/$LOGFILES/$i;
 done
 
